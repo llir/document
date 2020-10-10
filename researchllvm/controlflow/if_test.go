9a7e634787ad8bf5d558a2749441da26c4fd000f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-10-10 13:49:18 +0800

    if done

--- researchllvm/controlflow/if_test.go
+++ researchllvm/controlflow/if_test.go
@@ -12,6 +12,7 @@ import (
 )
 
 type Expr interface{ isExpr() Expr }
+type EVoid struct{ Expr }
 type EBool struct {
 	Expr
 	V bool
@@ -26,11 +27,12 @@ type Stmt interface{ isStmt() Stmt }
 type SIf struct {
 	Stmt
 	Cond Expr
+	Then Stmt
+	Else Stmt
 }
-type SAssign struct {
+type SRet struct {
 	Stmt
-	VarName string
-	Val     Expr
+	Val Expr
 }
 
 func compileExpr(b *ir.Block, e Expr) value.Value {
@@ -42,27 +44,42 @@ func compileExpr(b *ir.Block, e Expr) value.Value {
 			return constant.NewInt(types.I1, 0)
 		}
 	case *EEq:
-		b.NewICmp(enum.IPredEQ, compileExpr(b, e.L), compileExpr(b, e.R))
+		return b.NewICmp(enum.IPredEQ, compileExpr(b, e.L), compileExpr(b, e.R))
+	case *EVoid:
+		return nil
 	}
 	panic("unknown expression")
 }
 
-func compileIf(f *ir.Func, ifStmt *SIf) {
-	b := f.NewBlock("if")
-	cond := compileExpr(b, ifStmt.Cond)
-	tru := f.NewBlock("if-then")
-	fls := f.NewBlock("if-else")
-	b.NewCondBr(cond, tru, fls)
-	tru.NewRet(nil)
-	fls.NewRet(nil)
+func compileStmt(f *ir.Func, bb *ir.Block, stmt Stmt) {
+	switch s := stmt.(type) {
+	case *SIf:
+		thenB := f.NewBlock("")
+		compileStmt(f, thenB, s.Then)
+		elseB := f.NewBlock("")
+		compileStmt(f, elseB, s.Else)
+		bb.NewCondBr(compileExpr(bb, s.Cond), thenB, elseB)
+		if thenB.Term == nil {
+			leaveB := f.NewBlock("")
+			thenB.NewBr(leaveB)
+		}
+	case *SRet:
+		bb.NewRet(compileExpr(bb, s.Val))
+	}
 }
 
 func TestParameterAttr(t *testing.T) {
 	f := ir.NewFunc("foo", types.Void)
+	bb := f.NewBlock("")
 
-	compileIf(f, &SIf{
+	compileStmt(f, bb, &SIf{
 		Cond: &EBool{V: true},
+		Then: nil,
+		Else: &SRet{Val: &EVoid{}},
 	})
+
+	// whatever what we did in compileStmt, we use convention that a block leave in the end is empty.
+	f.Blocks[len(f.Blocks)-1].NewRet(nil)
 
 	fmt.Println(f.LLString())
 }
@@ -70,7 +87,17 @@ func TestParameterAttr(t *testing.T) {
 // generated LLVM IR:
 //
 // ```
-// %Foo = type { i32 }
+// define void @foo() {
+// ; <label>:0
+// br i1 true, label %1, label %2
 //
-// declare void @foo(%Foo noalias sret %result)
+// ; <label>:1
+// br label %3
+//
+// ; <label>:2
+// ret void
+//
+// ; <label>:3
+// ret void
+// }
 // ```
