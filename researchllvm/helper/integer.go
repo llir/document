15d1f5dab444d3deb84a41a78b91574ab4bbbdc0
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-14 02:15:04 +0800

    constant int for convenience

---
+++ researchllvm/helper/integer.go
@@ -0,0 +1,11 @@
+package helper
+
+import (
+	"github.com/llir/llvm/ir/constant"
+	"github.com/llir/llvm/ir/types"
+)
+
+func CI8(x int64) constant.Constant  { return constant.NewInt(types.I8, x) }
+func CI16(x int64) constant.Constant { return constant.NewInt(types.I16, x) }
+func CI32(x int64) constant.Constant { return constant.NewInt(types.I32, x) }
+func CI64(x int64) constant.Constant { return constant.NewInt(types.I64, x) }
