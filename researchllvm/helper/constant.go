c1026254a00dba536e31363f5f46b7774ce38524
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-14 03:30:19 +0800

    constant f32 f64

--- researchllvm/helper/constant.go
+++ researchllvm/helper/constant.go
@@ -9,3 +9,6 @@ func CI8(x int64) constant.Constant  { return constant.NewInt(types.I8, x) }
 func CI16(x int64) constant.Constant { return constant.NewInt(types.I16, x) }
 func CI32(x int64) constant.Constant { return constant.NewInt(types.I32, x) }
 func CI64(x int64) constant.Constant { return constant.NewInt(types.I64, x) }
+
+func CF32(x float64) constant.Constant { return constant.NewFloat(types.Float, x) }
+func CF64(x float64) constant.Constant { return constant.NewFloat(types.Double, x) }
